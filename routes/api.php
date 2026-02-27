986c3139ee1ff133329df320e0b02a1c28c8e9c6
AmalJoeSevin <amaljoesevin@gmail.com>
2026-02-28 00:30:34 +0530

    Routes added for drivers

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,20 @@ Route::get('/test', function () {
 Route::get('/driver', [DriverController::class, 'index']);
 Route::post('/drivers', [DriverController::class, 'store']);
 
-Route::apiResource('drivers', DriverController::class);
+//Route::apiResource('drivers', DriverController::class);
+
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::apiResource('drivers', DriverController::class);
+//});
+
 Route::get('drivers-explain', [DriverController::class,'explainTest']);
+
+Route::prefix('drivers')->group(function () {
+
+    Route::get('/', [DriverController::class, 'index']);        // List all drivers
+    Route::post('/', [DriverController::class, 'store']);       // Create driver
+    Route::get('/{id}', [DriverController::class, 'show']);     // Single driver
+    Route::put('/{id}', [DriverController::class, 'update']);   // Update driver
+    Route::delete('/{id}', [DriverController::class, 'destroy']); // Delete driver
+
+});
